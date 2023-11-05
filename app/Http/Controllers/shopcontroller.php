0672a9a82670396eb56e4160a7079ccde922a284
igacuhi <igacuhi@gmail.com>
2023-11-05 23:31:03 +0300

    shopcontroller.php

--- app/Http/Controllers/shopcontroller.php
+++ app/Http/Controllers/shopcontroller.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class shopcontroller extends Controller
 {
     public function index(){
-        $products = \App\Models\Product::orderBy('created_at','DESC')->paginage(12);
-        return view('shop',[$products]);
+        $products = \App\Models\Product::orderBy('created_at','DESC')->paginate(12);
+        return view('shop', compact('products'));
+
     }
 }
