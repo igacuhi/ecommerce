e70ed8ee57fc0a15154a7144bd2894d8bd011985
igacuhi <igacuhi@gmail.com>
2023-11-18 12:19:52 +0300

    calling the js fuction(onclick)

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -95,7 +95,7 @@
                                 <h2 class="td-color">${{$item->subtotal()}}</h2>
                             </td>
                             <td>
-                                <a href="javascript:void(0)">
+                                <a href="javascript:void(0)" onclick="removeItemFromCart('{{$item->rowId}}')">
                                     <i class="fas fa-times"></i>
                                 </a>
                             </td>
@@ -108,7 +108,7 @@
                 <div class="row">
                     <div class="col-sm-7 col-5 order-1">
                         <div class="left-side-button text-end d-flex d-block justify-content-end">
-                            <a href="javascript:void(0)"
+                            <a href="javascript:void(0)" onclick="clearCart()"
                                 class="text-decoration-underline theme-color d-block text-capitalize">clear
                                 all items</a>
                         </div>
