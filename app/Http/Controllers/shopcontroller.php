6db84ac019f7502a9dd75af23a86358c2997b3f9
igacuhi <igacuhi@gmail.com>
2023-11-20 18:24:00 +0300

    updating

--- app/Http/Controllers/shopcontroller.php
+++ app/Http/Controllers/shopcontroller.php
@@ -3,19 +3,17 @@
 namespace App\Http\Controllers;
 use App\Models\Product;
 use Gloudemans\Shoppingcart\Facades\Cart; // Replace "App" with the actual namespace
-
-
 use Illuminate\Http\Request;
 
 class shopcontroller extends Controller
 {
-    public function index(Request $request){
+    public function index(){
         $products = Product::orderBy('created_at','DESC')->paginate(12);
         return view('shop', compact('products'));
 
     }
     public function productDetails($slug){
-        $product = product::where('slug','')->first();
+        $product = product::where('slug','$slug')->first();
         $rproducts=product::where('slug','!=',$slug)->inRandomOrder('id')->get()->take(8); 
         return view('details',['product'=>$product,'rproducts'=>$rproducts]);
 
