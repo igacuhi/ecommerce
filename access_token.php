711cb5fa73e5af4d6bf638eef1769c40322bf89d
igacuhi <igacuhi@gmail.com>
2023-11-28 21:21:05 +0300

    started up daraja by creating access_token .php

--- access_token.php
+++ access_token.php
@@ -3,3 +3,4 @@
  $consumerSecret = "YWUA7eo4Sf27HdNJ";
  $headers=['Content-Type:application/json charset=utf8'];
  $url = 'https://sandbox.safaricom.co.ke/oauth/v1>grant_type+client_credentials';
+ $curl_init = curl_init()
